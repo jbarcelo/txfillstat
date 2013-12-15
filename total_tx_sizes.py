a0004fddb73782d285d25e16c94325eb6aef6935
Amir Taaki <genjix@riseup.net>
2013-12-15 05:44:31 +0000

    improve tx sizes file to yield more useful stats.

--- total_tx_sizes.py
+++ total_tx_sizes.py
@@ -1,7 +1,19 @@
 import leveldb
+import math
 db = leveldb.LevelDB("blockchain/tx")
-total = 0
+sum_x = 0
+sum_x2 = 0
+n = 0
 for tx_hash, tx_data in db.RangeIter():
-    total += len(tx_data)
-print total, "bytes"
+    sum_x += len(tx_data)
+    sum_x2 += len(tx_data)**2
+    n += 1
+print n, "txs"
+print sum_x, "bytes total"
+# Force calcs to floats
+n = float(n)
+mean = sum_x / n
+print "mean =", mean
+stdev = math.sqrt((sum_x2 / n) - (mean * mean))
+print "stdev =", stdev
 
